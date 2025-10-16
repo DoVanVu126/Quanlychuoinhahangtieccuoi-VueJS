f9c9b18306fc327c0586ce2a6f72d13c66e549a1
DoVanVu126 <dovu1262004@gmail.com>
2025-10-16 10:43:31 +0700

    giao dien

---
+++ bootstrap-vue-argon-dashboard-master/src/routes/routes.js
@@ -0,0 +1,76 @@
+// router/routes.js
+import DashboardLayout from '@/views/Layout/DashboardLayout.vue';
+import AuthLayout from '@/views/Pages/AuthLayout.vue';
+import NotFound from '@/views/NotFoundPage.vue';
+
+const routes = [
+  {
+    path: '/',
+    redirect: 'dashboard',
+    component: DashboardLayout,
+    children: [
+      {
+        path: '/dashboard',
+        name: 'dashboard',
+        component: () => import(/* webpackChunkName: "demo" */ '../views/Dashboard.vue')
+      },
+      {
+        path: '/icons',
+        name: 'icons',
+        component: () => import(/* webpackChunkName: "demo" */ '../views/Icons.vue')
+      },
+      {
+        path: '/profile',
+        name: 'profile',
+        component: () => import(/* webpackChunkName: "demo" */ '../views/Pages/UserProfile.vue')
+      },
+      {
+        path: '/maps',
+        name: 'maps',
+        component: () => import(/* webpackChunkName: "demo" */ '../views/GoogleMaps.vue')
+      },
+      {
+        path: '/tables',
+        name: 'tables',
+        component: () => import(/* webpackChunkName: "demo" */ '../views/RegularTables.vue')
+      },
+
+      // 👉 QUẢN LÝ MÓN ĂN
+      {
+        path: '/mon-an',
+        name: 'MonAn',
+        component: () => import('../views/MonAn.vue')
+      },
+      {
+        path: '/mon-an/them',
+        name: 'ThemMonAn',
+        component: () => import('../views/ThemMonAn.vue')
+      },
+      {
+        path: '/mon-an/sua/:id',
+        name: 'SuaMonAn',
+        component: () => import('../views/SuaMonAn.vue')
+      }
+    ]
+  },
+  {
+    path: '/',
+    redirect: 'login',
+    component: AuthLayout,
+    children: [
+      {
+        path: '/login',
+        name: 'login',
+        component: () => import(/* webpackChunkName: "demo" */ '../views/Pages/Login.vue')
+      },
+      {
+        path: '/register',
+        name: 'register',
+        component: () => import(/* webpackChunkName: "demo" */ '../views/Pages/Register.vue')
+      },
+      { path: '*', component: NotFound }
+    ]
+  }
+];
+
+export default routes;
